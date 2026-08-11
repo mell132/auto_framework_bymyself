a3d97a110ee45312c08ebb5c9272575bad8175d7
mell132 <18946517668@163.com>
2026-08-11 16:57:16 +0800

    修改虚拟机ip，添加http断言

--- testcases/test_runner.py
+++ testcases/test_runner.py
@@ -1,17 +1,40 @@
 import pytest
 import requests
+import jsonpath
 from utils.excel_utils import read_excel
 
 class TestRunner:
     #读取测试用例文件中的全部数据，用属性保存
     data=read_excel()
+    #提取后的数据需要初始化一个全局的属性来保存，可以使用{}空字典
+    all={}
 
     @pytest.mark.parametrize("case",data)
     def test_case(self,case):
         #解析请求数据
+        method = case["method"]
+        url = "http://192.168.10.131:8888/api/private/v1"+case["path"]
+        headers=eval(case["headers"]) if isinstance(case["headers"],str) else None
+        params=eval(case["params"]) if isinstance(case["params"],str) else None
+        data=eval(case["data"]) if isinstance(case["data"],str) else None
+        json=eval(case["json"]) if isinstance(case["json"],str) else None
+        files=eval(case["files"]) if isinstance(case["files"],str) else None
 
+        requests_data={
+            "method":method,
+            "url":url,
+            "headers":headers,
+            "params":params,
+            "data":data,
+            "json":json,
+            "files":files,
+        }
         #发送请求，获得响应结果
-
+        res=requests.request(**requests_data)
         #处理断言
+        if case["check"]:
+            assert jsonpath.jsonpath(res.json(),case["check"])[0]==case["expected"]
+        else:
+            assert case["expected"] in res.text
 
         #提取
--- utils/excel_utils.py
+++ utils/excel_utils.py
@@ -2,7 +2,7 @@ import openpyxl
 
 def read_excel():
     #打开excel文件
-    workbook=openpyxl.load_workbook("./data/登录.xlsx")
+    workbook=openpyxl.load_workbook("./data/测试用例.xlsx")
 
     #选择表
     worksheet=workbook["Sheet1"]
@@ -11,7 +11,9 @@ def read_excel():
     keys=[cell.value for cell in worksheet[2]]
     for row in worksheet.iter_rows(min_row=3, values_only=True): #从第三行开始，只返回值
         dict_data=dict(zip(keys, row))
-        data.append(dict_data)
+        #如果读取为true，append，否则不append
+        if dict_data["is_true"]:
+            data.append(dict_data)
     #关闭excel
     workbook.close()
     return data
